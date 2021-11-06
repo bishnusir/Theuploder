ae6ffffd859459d68d5ebc85435293f51ac9f235
bishnusir <85861363+bishnusir@users.noreply.github.com>
2021-11-06 08:38:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,4 @@
-<?php
-if (isset($_POST["submit"])) {
-  $file_url = $_POST["file_url"];
-  $file_name = $_POST["file_name"];
 
-  if (filter_var($file_url, FILTER_VALIDATE_URL)) {
-    $pathinfo = pathinfo($file_url);
-    if (isset($pathinfo["extension"])) {
-      $file_name .= "." . $pathinfo["extension"];
-      $file_content = file_get_contents($file_url);
-      $fopen = fopen("files/" . $file_name, "w");
-      $fwrite = fwrite($fopen, $file_content);
-      fclose($fopen);
-      if ($fwrite) {
-        $msg["success"] = "File Uploaded Successfully.";
-        $_POST["file_url"] = "";
-        $_POST["file_name"] = "";
-      } else {
-        $msg["error"] = "Something wrong went. Please try again.";
-      }
-    } else {
-      $msg["error"] = "Please enter a file URL.";
-    }
-  } else {
-    $msg["error"] = "Please enter a valid URL.";
-  }
-}
-?>
 <!DOCTYPE html>
 <!--
   Code by Pure Coding
@@ -34,6 +7,89 @@ if (isset($_POST["submit"])) {
 -->
 <html lang="en">
   <head>
+    <style>
+    @import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Roboto", sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #0b79ea;
+  min-height: 100vh;
+}
+
+.container {
+  max-width: 400px;
+  width: 100%;
+  background: white;
+  padding: 2rem;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
+}
+
+.container .title {
+  font-size: 1.7rem;
+  font-weight: 800;
+  border-left: 0.3rem solid;
+  padding-left: 0.5rem;
+  color: #0b79ea;
+  margin-bottom: 1rem;
+  text-transform: uppercase;
+  word-spacing: 4px;
+}
+
+.container .inputBox {
+  margin-bottom: 1rem;
+}
+
+.container .inputBox:last-child {
+  margin-bottom: 0;
+}
+
+.container .inputBox .form-label {
+  display: block;
+  font-weight: 500;
+  font-size: 0.9rem;
+  color: #222;
+  margin-bottom: 0.3rem;
+}
+
+.container .inputBox .form-control {
+  border: 1px solid #eee;
+  width: 100%;
+  min-height: 40px;
+  padding: 1rem;
+  font-size: 0.9rem;
+  color: #222;
+  outline: none;
+  transition: 0.3s;
+}
+
+.container .inputBox .form-control:focus,
+.container .inputBox .form-control:valid {
+  border-color: #0b79ea;
+}
+
+.btn {
+  border: none;
+  outline: none;
+  background: #0b79ea;
+  padding: 0.7rem 2rem;
+  color: white;
+  font-weight: 500;
+  font-size: 0.9rem;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
+  text-decoration: none;
+  cursor: pointer;
+}
+
+    </style>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -83,3 +139,31 @@ if (isset($_POST["submit"])) {
     </div>
   </body>
 </html>
+<?php
+if (isset($_POST["submit"])) {
+  $file_url = $_POST["file_url"];
+  $file_name = $_POST["file_name"];
+
+  if (filter_var($file_url, FILTER_VALIDATE_URL)) {
+    $pathinfo = pathinfo($file_url);
+    if (isset($pathinfo["extension"])) {
+      $file_name .= "." . $pathinfo["extension"];
+      $file_content = file_get_contents($file_url);
+      $fopen = fopen("files/" . $file_name, "w");
+      $fwrite = fwrite($fopen, $file_content);
+      fclose($fopen);
+      if ($fwrite) {
+        $msg["success"] = "File Uploaded Successfully.";
+        $_POST["file_url"] = "";
+        $_POST["file_name"] = "";
+      } else {
+        $msg["error"] = "Something wrong went. Please try again.";
+      }
+    } else {
+      $msg["error"] = "Please enter a file URL.";
+    }
+  } else {
+    $msg["error"] = "Please enter a valid URL.";
+  }
+}
+?>
